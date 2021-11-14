ff5ff92e2fa6af17b9a44d417c4f519b67c60ec8
Perry van Velzen <perryvvelzen@gmail.com>
2021-11-14 18:58:38 +0100

    asd

---
+++ WebApplication3/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3
+{
+    public class TestModel
+    {
+        public string TestStringKey { get; set; }
+        public decimal? TestNumericKey { get; set; }
+        public List<TestModel> TestListKey { get; set; }
+        public bool? TestBooleanKey { get; set; }
+        public TestEnum? TestEnum { get; set; }
+    }
+
+    public enum TestEnum
+    {
+        TestValue1,
+        TestValue2
+    }
+}
